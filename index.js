622bd14468b1aa6a47fa95c4a2f6cc11b1022cdd
Joel Eguiza <joeleg96@gmail.com>
2025-05-21 16:39:35 -0600

    added functionality to photo gallery

---
+++ index.js
@@ -0,0 +1,62 @@
+var productImgList = [".product-one", ".product-two", ".product-three", ".product-four"];
+var globalClickTracker = 0;
+
+$(".thumbnail").on("click", function(event) {
+    $(".thumbnail").removeClass("filter");
+    $(this).addClass("filter");
+
+    switch (this.id) {
+        case "thumbnail-one":
+            $(".product-img").addClass("hide");
+            $(".product-one").removeClass("hide");
+            globalClickTracker = 0;
+            break;
+
+        case "thumbnail-two":
+            $(".product-img").addClass("hide");
+            $(".product-two").removeClass("hide");
+            globalClickTracker = 1;
+            break;
+
+        case "thumbnail-three":
+            $(".product-img").addClass("hide");
+            $(".product-three").removeClass("hide");
+            globalClickTracker = 2;
+            break;
+
+        case "thumbnail-four":
+            $(".product-img").addClass("hide");
+            $(".product-four").removeClass("hide");
+            globalClickTracker = 3;
+            break;
+    
+        default:
+            break;
+    }
+})
+
+$(".next-btn").on("click", function() {
+    globalClickTracker += 1;
+    $(".product-img").addClass("hide");
+    $(productImgList[globalClickTracker]).removeClass("hide");
+    console.log(globalClickTracker);
+    
+    if(globalClickTracker === 4) {
+        globalClickTracker = 0;
+        $(".product-img").addClass("hide");
+        $(productImgList[globalClickTracker]).removeClass("hide");
+    }
+});
+
+$(".previous-btn").on("click", function() {
+    globalClickTracker -= 1;
+    $(".product-img").addClass("hide");
+    $(productImgList[globalClickTracker]).removeClass("hide");
+    console.log(globalClickTracker);
+    
+    if(globalClickTracker === -1) {
+        globalClickTracker = 3;
+        $(".product-img").addClass("hide");
+        $(productImgList[globalClickTracker]).removeClass("hide");
+    }
+});
